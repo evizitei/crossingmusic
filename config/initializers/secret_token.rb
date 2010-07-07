27123ccdf8f7d7bd2bce82887b17dba7bc01d0f2
Ethan Vizitei <ethan.vizitei@gmail.com>
2010-07-07 16:24:10 -0500

    [COMPLETE #4188855 #4188850] Initial project structure
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '1642c7654b388b0c0b11b3bddc3ad4c3a41eb8b9af7c049b8535d27bf62e9b487f63ba4758563d4684ab4a226876fde22628d9ccbd22752b8591d4cb21a97058'
