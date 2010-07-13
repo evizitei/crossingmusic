573a67b70e56a4851de29cf9280a071282892c3e
Ethan Vizitei <ethan.vizitei@gmail.com>
2010-07-13 10:52:38 -0500

    add vote type scopes and validation to song vote model
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

--- app/models/song_vote.rb
+++ app/models/song_vote.rb
@@ -1,3 +1,10 @@
 class SongVote < ActiveRecord::Base
   belongs_to :song
+  
+  scope :positive, where(:vote=>"YES")
+  scope :negative, where(:vote=>"NO")
+  scope :neutral, where(:vote=>"MEH")
+  
+  validates :vote,   :presence=>true
+  validates :song_id,:presence=>true
 end
--- test/unit/song_vote_test.rb
+++ test/unit/song_vote_test.rb
@@ -2,12 +2,13 @@ require 'test_helper'
 
 class SongVoteTest < Test::Unit::TestCase
   should_belong_to :song
-  
+  should_validate_presence_of :vote,:song_id
+
   context "vote direction scope" do
     setup do
       @positive_vote = Factory(:positive_vote)
-      @negative_vote = Factory(:positive_vote)
-      @nuetral_vote = Factory(:positive_vote)
+      @negative_vote = Factory(:negative_vote)
+      @neutral_vote = Factory(:neutral_vote)
     end
     
     context "for positive votes" do
@@ -17,14 +18,50 @@ class SongVoteTest < Test::Unit::TestCase
     end
     
     context "for negative votes" do
-      setup do
-        @votes = SongVote.negative.all
+      setup { @votes = SongVote.negative.all }
+      
+      should "include negative vote" do
+        assert_contains @votes,@negative_vote
+      end
+      
+      should "not include neutral vote" do
+        assert_does_not_contain @votes,@neutral_vote
+      end
+      
+      should "not include positive vote" do
+        assert_does_not_contain @votes,@positive_vote
       end
     end
     
     context "for positive votes" do
-      setup do
-        @votes = SongVote.neutral.all
+      setup { @votes = SongVote.positive.all }
+      
+      should "not include negative vote" do
+        assert_does_not_contain @votes,@negative_vote
+      end
+      
+      should "not include neutral vote" do
+        assert_does_not_contain @votes,@neutral_vote
+      end
+      
+      should "include positive vote" do
+        assert_contains @votes,@positive_vote
+      end
+    end
+    
+    context "for neutral votes" do
+      setup { @votes = SongVote.neutral.all }
+      
+      should "not include negative vote" do
+        assert_does_not_contain @votes,@negative_vote
+      end
+      
+      should "not include neutral vote" do
+        assert_contains @votes,@neutral_vote
+      end
+      
+      should "not include positive vote" do
+        assert_does_not_contain @votes,@positive_vote
       end
     end
   end
