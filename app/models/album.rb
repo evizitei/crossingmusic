9a9e8bf53862c1cb03ea23abf41ab2035ab68d45
Ethan Vizitei <ethan.vizitei@gmail.com>
2011-06-04 10:23:25 -0500

    add thumbnails for album cover
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,7 @@ class Album < ActiveRecord::Base
   has_many :songs
 
   has_attached_file :cover, 
+                    :styles => { :thumb => "100x100>"},
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root.to_s}/config/amazon_s3.yml",
                     :s3_permissions => :public_read,
